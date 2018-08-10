56417cd5fed89342db36bdfbcfbc62c1dedef4ff
KerelOlivier <olivierboeren@live.nl>
2018-08-11 01:26:24 +0200

    Player Movement Grid Based
    Added
    +prefabs folder
      -Added a tile prefab
    +player gameobject
    +gamemanager
    +movement script
      -has direction input
    +sprites for the player and tile
      -placeholders

---
+++ LudumDare42/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+enum Directions { North, East, South, West}
+public class PlayerController : MonoBehaviour {
+    SpriteRenderer sr;      //reference to the sprite renderer
+    public Sprite[] sprites = new Sprite[4];   //Directional sprites, can be adjusted for extra player sprites
+    float speed = .15f;     //MovementSpeed
+    float timer = 0;        //Timer for the next movement  
+    Directions dir = Directions.North;         //The direction in which the player is facing
+    private void Start() {
+        sr = this.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update() {
+        timer += Time.deltaTime;    //Add the time between frames to the timer
+        //Move the player if the timer has run out
+        if (Input.GetKey(KeyCode.W) && timer >= speed) {
+            if (this.transform.position.y < GameManager.height - 1) {
+                this.transform.position += Vector3.up;
+                sr.sprite = sprites[0];
+                dir = Directions.North;
+                timer = 0;
+            }                
+        } else if (Input.GetKey(KeyCode.S) && timer >= speed) {
+            if (this.transform.position.y > 0) {
+                this.transform.position += Vector3.down;
+                sr.sprite = sprites[2];
+                dir = Directions.South;
+                timer = 0;
+            }
+        } else if (Input.GetKey(KeyCode.A) && timer >= speed) {
+            if (this.transform.position.x > 0) {
+                this.transform.position += Vector3.left;
+                sr.sprite = sprites[3];
+                dir = Directions.West;
+                timer = 0;
+            }                
+        } else if (Input.GetKey(KeyCode.D) && timer >= speed) {
+            if (this.transform.position.x < GameManager.width - 1) {
+                this.transform.position += Vector3.right;
+                sr.sprite = sprites[1];
+                dir = Directions.East;
+                timer = 0;
+            }                
+        }        
+    }
+}
