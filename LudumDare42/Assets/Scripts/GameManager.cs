5e8d8c81ec82a40cb7f2058567155bcf01f32488
KerelOlivier <olivierboeren@live.nl>
2018-08-11 15:49:24 +0200

    Added highscore system
    Yet to be implemented

--- LudumDare42/Assets/Scripts/GameManager.cs
+++ LudumDare42/Assets/Scripts/GameManager.cs
@@ -1,24 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-	public static int width = 10;				//width of the grid
-	public static int height =10;				//height of the grid
+    public static int width = 10;               //width of the grid
+    public static int height = 10;				//height of the grid
     public GameObject tile;     //Tile GameObject
     public static int[,] grid;	//The grid where all the info about the boxes and players is stored
     public static GameObject[,] tiles;  //The tile grid
-	
+    public Text text;                   //Publix input for textbox score
+    public static Text scoreText;      //Textbox where the score will be displayed
+
     //delay stuff for spawning the boxes
     public static float boxSpawnDelay = 5f;
     float currentSpawnDelay = 5f;
 
-    public Transform boxesParent;
-    
+    //Scores
+    private static int score = 0;    //Current score
+    public static int Score {      //Score editor
+        get {
+            return score;
+        }
+        set {
+            score += value;                     //Set the score
+            scoreText.text = score.ToString();  //Update Label
+            //Debug.Log(score);
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		/*grid = new int[width, height];
+    // Use this for initialization
+    void Start() {
+        scoreText = text;
+        /*grid = new int[width, height];
         tiles = new GameObject[width, height];
 
          
@@ -29,30 +43,31 @@ public class GameManager : MonoBehaviour {
                 grid[x, y] = 0;
             }
         }*/
-	}
-	
-	// Update is called once per frame
-<<<<<<< HEAD
-	void Update () 
-    {
-		currentSpawnDelay -= Time.deltaTime;
+        //Start lineair time score icrementing
+        InvokeRepeating("IncrementScore", 1, 1);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        currentSpawnDelay -= Time.deltaTime;
         if (currentSpawnDelay < 0) {
             GenerateBoxes(3);
             currentSpawnDelay = boxSpawnDelay;
         }
-=======
-	void Update () {
-		
->>>>>>> Art
-
-	}
+    }
 
     //generates the boxes
-    void GenerateBoxes(int amount)
-    {
-        for (int i = 0; i < amount; i++)
-        {
-            Box.SpawnAt(new Vector2Int(Random.Range(0,width), Random.Range(0,height)), Box.boxTypes[Random.Range(0,Box.boxTypes.Length - 1)], boxesParent);
+    void GenerateBoxes(int amount) {
+        for (int i = 0; i < amount; i++) {
+            Box.SpawnAt(new Vector2Int(Random.Range(0, width), Random.Range(0, height)), Box.boxTypes[Random.Range(0, Box.boxTypes.Length - 1)], this.transform);
         }
+    }
+
+    //Increments the score every so many seconds
+    void IncrementScore() {
+        score += 1;
+        scoreText.text = score.ToString();
+        //Debug.Log("IncrementScore by 1");
     }
 }
---
+++ LudumDare42/Assets/Scripts/ScoreBoardManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoardManager : MonoBehaviour {
+
+    public static List<Score> scores = new List<Score>();
+
+    private static void Awake() {
+        //Load scores from save
+        scores.Add(new Score(PlayerPrefs.GetInt("hs0"), PlayerPrefs.GetString("hsn0")));
+        scores.Add(new Score(PlayerPrefs.GetInt("hs1"), PlayerPrefs.GetString("hsn1")));
+        scores.Add(new Score(PlayerPrefs.GetInt("hs2"), PlayerPrefs.GetString("hsn2")));
+        scores.Add(new Score(PlayerPrefs.GetInt("hs3"), PlayerPrefs.GetString("hsn3")));
+        scores.Add(new Score(PlayerPrefs.GetInt("hs4"), PlayerPrefs.GetString("hsn4")));
+    }
+
+    public void SaveScore(Score score) {
+        if(score.score > scores[4].score) {
+            if(score.score > scores[3].score) {
+                if (score.score > scores[2].score) {
+                    if (score.score > scores[1].score) {
+                        if (score.score > scores[0].score) {
+                            scores[0] = score;
+                        } else {
+                            scores[1] = score;
+                        }
+                    } else {
+                        scores[2] = score;
+                    }
+                } else {
+                    scores[3] = score;
+                }
+            } else {
+                scores[4] = score;
+            }
+        }
+    }
+    public class Score {
+        public int score = 0;
+        public string name = "";
+
+        public Score(int score, string name) {
+            this.score = score;
+            this.name = name;
+        }
+    }
+}
